c6dd5488a0ee4379c3245ac7e04a123530a47b92
Michelle C. Funk <michelle@michellefunk.com>
2015-04-30 17:13:43 -0500

    Adjusted to query JSON only once

--- script.js
+++ script.js
@@ -4,29 +4,21 @@ var cityName, cityPop, cityArea, cityDensity;
 
 
 $(document).ready(function () {
-
-    $("#get-city-btn").on("click", function() {
-        if (numCities == 0) {
-            $.get('cities.html', function (data) {
-                $("#cityList").append(data);
-            });
-        } else if (numCities == 5) {
-            numCities = 0;
-            $(".citiesListed").empty();
-        }
-
-        $.get('data.json', function (data) {
+    $.get('cities.html', function (data) {
+        $("#cityList").append(data);
+    });
+    $.get('data.json', function (data) {
             console.log(".get data function is running");
             cityData = data;
-            cityName = cityData.cities[numCities].name;
-            cityPop = cityData.cities[numCities].population;
-            cityArea = cityData.cities[numCities].landArea;
-            cityDensity = cityData.cities[numCities].density;
-            console.log(cityName, cityPop, cityArea, cityDensity);
-            $('.citiesListed').append("<div class='oneCity'><p>Name: " + cityName + "</p><p>Population: " + cityPop +
-                    "</p><p>Land Area: " + cityArea + "</p><p>Density: " + cityDensity + "</p></div>");
-            numCities++;
-        });
+    });
+
+    $("#get-city-btn").on("click", function() {
+        if (numCities == 5) {
+            numCities = 0;
+            $(".citiesListed").empty();
+        } else {
+            addCity();
+        }
     });
 
     $("#cityList").on("click", ".button-remove", function() {
@@ -34,5 +26,16 @@ $(document).ready(function () {
         $(".citiesListed").empty();
         numCities = 0;
     });
+
+    function addCity() {
+        cityName = cityData.cities[numCities].name;
+        cityPop = cityData.cities[numCities].population;
+        cityArea = cityData.cities[numCities].landArea;
+        cityDensity = cityData.cities[numCities].density;
+        console.log(cityName, cityPop, cityArea, cityDensity);
+        $('.citiesListed').append("<div class='oneCity'><p>Name: " + cityName + "</p><p>Population: " + cityPop +
+            "</p><p>Land Area: " + cityArea + "</p><p>Density: " + cityDensity + "</p></div>");
+        numCities++;
+    }
 });
 
